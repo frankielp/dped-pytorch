72a35812b8c6b4bcc676f387864bd7c3ba86d4ce
frankielp <frankiecomrade@gmail.com>
2023-05-31 19:07:02 +0000

    gan

--- script/task.sh
+++ script/task.sh
@@ -2,15 +2,15 @@
 #SBATCH --job-name=test          # create a short name for your job
 #SBATCH --nodes=1                # node count
 #SBATCH --ntasks=1               # total number of tasks across all nodes
-#SBATCH --cpus-per-task=8        # cpu-cores per task (>1 if multi-threaded tasks)
-#SBATCH --mem-per-cpu=2G         # memory per cpu-core (4G is default)
+#SBATCH --cpus-per-task=2        # cpu-cores per task (>1 if multi-threaded tasks)
+#SBATCH --mem-per-cpu=8G         # memory per cpu-core (4G is default)
 #SBATCH --time=5-00:00:00          # total run time limit (HH:MM:SS)
 #SBATCH --mail-type=begin        # send email when job begins
 #SBATCH --mail-type=end          # send email when job ends
 #SBATCH --mail-user=<YourNetID>@princeton.edu
 #Number of GPUs, this can be in the format of "gpu:[1-4]", or "gpu:K80:[1-4] with the type included
 #SBATCH --gres=gpu:1
-#SBATCH --nodelist=selab4
+#SBATCH --nodelist=selab2
 #SBATCH -o/home/lpnquynh/desktop/dped-pytorch/script/test.out
 #SBATCH -e/home/lpnquynh/desktop/dped-pytorch/script/test.err
 
@@ -33,12 +33,21 @@ source activate cinnamon
 # # Verify install:
 # python3 -c "import tensorflow as tf; print(tf.config.list_physical_devices('GPU'))"
 
-cd ../ref
+cd ../utils
 ## TRAIN ##
 # python train_model.py batch_size=32 model=iphone dped_dir=dped/ vgg_dir=vgg_pretrained/imagenet-vgg-verydeep-19.mat
 
+## PREDICT
+# python test_model.py dped_dir=test_img/ iteration=18000 model=iphone test_subset=full resolution=orig use_gpu=true
+
+
 ## TEST
-python test_model.py dped_dir=test_img/ iteration=18000 model=iphone test_subset=full resolution=orig use_gpu=true
+cd ../ref
+python models.py
+
+cd ../utils
+python models.py
+
 
 conda deactivate
 
